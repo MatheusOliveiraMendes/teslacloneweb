7e287d4adc77f15f3f373414ab358a839e809c25
Matheus Mendes <41799217+MatheusOliveiraMendes@users.noreply.github.com>
2024-05-21 16:28:08 +0100

    Update

--- src/components/Page/styles.ts
+++ src/components/Page/styles.ts
@@ -3,41 +3,51 @@ import styled from 'styled-components';
 export const Container = styled.div`
 
  .colored:nth-child(1) {
-    background-image: url("img/Model_3.jpeg");
-background-position: center;  
-background-size: cover;
-}
-.colored:nth-child(2) {
     background-image: url("img/Model_Y.jpeg");
 background-position: center;  
 background-size: cover;
 }
-.colored:nth-child(3) {
-    background-image: url("img/Model_S.jpeg");
+.colored:nth-child(2) {
+    background-image: url("img/Model_3.jpeg");
 background-position: center;  
 background-size: cover;
 }
-.colored:nth-child(4) {
+.colored:nth-child(3) {
     background-image: url("img/Model_X.jpeg");
 background-position: center;  
 background-size: cover;
 }
+.colored:nth-child(4) {
+    background-image: url("img/Model_S.jpeg");
+background-position: center;  
+background-size: cover;
+}
 .colored:nth-child(5) {
-    background-image: url("img/Solar_Panels.jpeg");
+    background-image: url("img/Cybertruck.jpeg");
 background-position: center;  
 background-size: cover;
 }
 .colored:nth-child(6) {
-    background-image: url("img/Solar_Roof.jpeg");
+    background-image: url("img/Solar_Panels.jpeg");
 background-position: center;  
 background-size: cover;
 }
 .colored:nth-child(7) {
-    background-image: url("img/Accessories.jpeg");
+    background-image: url("img/Solar_Roof.jpeg");
 background-position: center;  
 background-size: cover;
 }
 .colored:nth-child(8) {
+    background-image: url("img/Powerwall.jpeg");
+background-position: center;  
+background-size: cover;
+}
+.colored:nth-child(9) {
+    background-image: url("img/Accessories.jpeg");
+background-position: center;  
+background-size: cover;
+}
+.colored:nth-child(10) {
 
 }
 `;
